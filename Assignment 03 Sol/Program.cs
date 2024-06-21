06d36a43c135e5f9d8f1ddbd60679cf318345efd
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-06-21 03:30:12 +0300

    Add project files.

---
+++ Assignment 03 Sol/Program.cs
@@ -0,0 +1,224 @@
+﻿namespace Assignment_03_Sol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num % 4 == 0 && num % 3 == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+            #endregion
+
+            #region Q2
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num > 0)
+            //    Console.WriteLine("Positive");
+            //else if (num < 0)
+            //    Console.WriteLine("Negative");
+            //else
+            //    Console.WriteLine("Equal Zero");
+            #endregion
+
+            #region Q3
+            //int x = int.Parse(Console.ReadLine()); 
+            //int y = int.Parse(Console.ReadLine()); 
+            //int z = int.Parse(Console.ReadLine()); 
+            //int max, min;
+            //if (x >= y && x >= z)
+            //    max = x;
+            //else if (y >= x && y >= z)
+            //    max = y;
+            //else
+            //    max = z;
+
+            //if(x <= y && x <= z)
+            //    min = x;
+            //else if (y <= x && y <= z)
+            //    min = y;
+            //else
+            //    min = z;
+
+            //Console.WriteLine($"Max element = {max}");
+            //Console.WriteLine($"Min element = {min}");
+            #endregion
+
+            #region Q4
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num % 2 == 0 ? "Even" : "Odd");
+            #endregion
+
+            #region Q5
+            //char c = Convert.ToChar(Console.ReadLine());
+            //if (c == 'a' || c == 'A' || c == 'o' || c == 'O' || c == 'u' || c == 'U' || c == 'e' || c == 'E' || c == 'i' || c == 'I')
+            //    Console.WriteLine("vowel");
+            //else
+            //    Console.WriteLine("Constant");
+            #endregion
+
+            #region Q6
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= num; i++)
+            //    Console.Write($"{i}, ");
+            #endregion
+
+            #region Q7
+            //int num = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //    Console.Write($"{num * i} ");
+            #endregion
+
+            #region Q8
+            //int num = int.Parse(Console.ReadLine());
+            //for(int i = 2; i <= num; i += 2)
+            //    Console.Write($"{i} ");
+            #endregion
+
+            #region Q9
+            //int BaseNum = Convert.ToInt32(Console.ReadLine());
+            //int Power = Convert.ToInt32(Console.ReadLine());
+            //int result = 1;
+            //for (int i = 0; i < Power; i++)
+            //{
+            //    result *= BaseNum;
+            //}
+            //Console.WriteLine(result); 
+            #endregion
+
+            #region Q10
+            //const int size = 5;
+            //int[] marks = new int[size];
+            //int sum = 0;
+            //double avg;
+            //for(int i = 0; i < size; i++)
+            //{
+            //    marks[i] = Convert.ToInt32(Console.ReadLine());
+            //    sum += marks[i];
+            //}
+            //avg = (double)sum / size;
+            //Console.WriteLine($"Total marks = {sum}");
+            //Console.WriteLine($"Average Marks = {avg}");
+            //Console.WriteLine($"Persentage = {avg / 100 * 100}%");
+            #endregion
+
+            #region Q11
+            //int month = int.Parse(Console.ReadLine());
+            //switch (month)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Days in Month: 28");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Days in Month: 30");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Days in Month: 30");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Days in Month: 30");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Days in Month: 30");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Days in Month: 31");
+            //        break;
+            //    default:
+            //        Console.WriteLine("invalid");
+            //        break;
+            //}
+            #endregion
+
+            #region Q12
+            //int num1 = int.Parse(Console.ReadLine());
+            //char op = char.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //if (op == '+')
+            //    Console.WriteLine(num1 + num2);
+            //else if (op == '-')
+            //    Console.WriteLine(num1 - num2);
+            //else if (op == '*')
+            //    Console.WriteLine(num1 * num2);
+            //else if (op == '/')
+            //    Console.WriteLine(num1 / num2);
+            //else
+            //    Console.WriteLine("Invalid operator");
+
+            #endregion
+
+            #region Q13
+            //// 01234
+            //// Ahmed  => demha
+            //string s = Console.ReadLine();
+
+            //for(int i = s.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(s[i]);
+            //}
+            #endregion
+
+            #region Q14
+            //int rev = 0;
+            //int num = int.Parse(Console.ReadLine()); // 1234
+            //while(num != 0)
+            //{
+            //    rev = rev * 10 + num % 10;
+            //    num /= 10;
+            //}
+            //Console.WriteLine(rev);
+            #endregion
+
+            #region Q15
+            //int stNum = int.Parse(Console.ReadLine());
+            //int enNum = int.Parse(Console.ReadLine());
+            //int count = 0;
+            //for (int i = stNum; i <= enNum; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    if (count == 2)
+            //        Console.Write($"{i} ");
+            //    count = 0;
+            //}
+            #endregion
+
+            #region Q16
+            int DecNum = int.Parse(Console.ReadLine());
+            int BinNum = 0, x = 1;
+            while (DecNum != 0)
+            {
+                BinNum = BinNum + (DecNum % 2) * x;
+                x *= 10;
+                DecNum = DecNum / 2;
+            }
+            Console.WriteLine(BinNum);
+            #endregion
+        }
+    }
+}
---
+++ Demo 03/Program.cs
@@ -0,0 +1,235 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Demo_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Fractions and Discards
+            //// Fractions
+            //double x = 3.5;  // by defualt any fraction takes data type double
+            //float y = 1.5f;
+            //decimal z = 2.0m;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            ////Discards ==> only just for readability
+            //long number = 100_000_000_000;
+            //Console.WriteLine(number);
+            #endregion
+
+            #region Implicit and Exeplicit casting 
+            //int X = 5;
+            //long Y = X; // implicit casting [safe casting]
+            //Console.WriteLine(Y);
+
+            //long x = 5;
+            //int y = (int)x;  Explicit casting [unsafe casting]
+            //Console.WriteLine(y);
+
+            //long x = 10000000000;
+            //int y = (int)x;
+            //Console.WriteLine(y); // y will store garbage
+
+            //long x = 10000000000;
+            //checked
+            //{
+            //    int y = (int) x;
+            //    unchecked
+            //    {
+            //        Console.WriteLine(y);
+            //    }
+            //}
+
+            //int x = 5;
+            //double y = x;  // implicit casting
+            //Console.WriteLine(y);
+
+            //double x = 5.5;
+            //int  y = (int)x;  // explicit casting 
+            //Console.WriteLine(y);   
+
+            #endregion
+
+            #region Operators
+            #region unary   ++ , --
+            ////1: postfix[X++, X--]
+            //int x = 10;
+            //Console.WriteLine(x++); // 10  [print then incremint] 
+            //Console.WriteLine(x--); // 11
+            //Console.WriteLine(x);   // 10
+
+            ////2: prefix [++X, --X]  
+            //int X = 10;
+            //Console.WriteLine(++X); // 11  [increment then print]
+            //Console.WriteLine(--X);
+            #endregion
+
+            #region Binary operator 
+            ////(+, -, *, /, %)
+            //int x = 10;
+            //int y = 5;
+            //Console.WriteLine(x + y);
+            //Console.WriteLine(x - y);
+            //Console.WriteLine(x * y);
+            //Console.WriteLine(x / y);
+            //Console.WriteLine(x % y);
+            #endregion
+
+            #region Assignment Operator
+            ////(=, -=, +=, *=, /=, %=)
+            //int x = 5;
+            //Console.WriteLine(x += 2);        // x = x + 2
+            //Console.WriteLine(x -= 2);        // x = x - 2
+            //Console.WriteLine(x *= 2);        // x = x * 2
+            //Console.WriteLine(x /= 2);        // x = x / 2
+            //Console.WriteLine(x %= 2);        // x = x % 2
+            #endregion
+
+            #region Logical Operators
+            //// (!, && short circt, ||)
+            //Console.WriteLine(!false);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true || false);
+            #endregion
+
+            #region Betwise operator
+            ////(& long circt, |)
+            //Console.WriteLine(true & false);
+            //Console.WriteLine(true | false);
+            #endregion
+
+            #region Ternary operator
+            //int x = 5, y = 6;
+            //string msg = (x > y ? "x is more than y" : x < y ? "x is less than y" : "x = y");
+            //Console.WriteLine(msg);
+            #endregion
+            #endregion
+
+            #region String formating
+            //int x = 5, y = 6, sum;
+            //sum = x + y;
+            //string msg;
+            //1: +
+            // msg = "The equation is: " + x + " + " + y + " = " + sum; // not recommended
+            //Console.WriteLine(msg);
+
+            //2: composit formating                                  //   {0} {1} {2}
+            // msg = string.Format("The equation is: {0} + {1} = {2}", x, y, sum);
+            //Console.WriteLine(msg);
+
+            //3: string interpolation
+            // manublation operator ($)
+            //msg = $"The equation is: {x} + {y} = {sum}";
+            //Console.WriteLine(msg);
+            #endregion
+
+            #region Control Statment (IF - Switch)
+            #region IF
+            //Console.WriteLine("Please Enter month number: ");
+            //int MonthNum = int.Parse(Console.ReadLine());
+            //if (MonthNum == 1)
+            //    Console.WriteLine("Hello Jenuary");
+            //else if (MonthNum == 2)
+            //    Console.WriteLine("Hello Febraury");
+            //else if (MonthNum == 3)
+            //    Console.WriteLine("Hello March");
+            //else if (MonthNum == 4)
+            //    Console.WriteLine("Hello April");
+            //else
+            //    Console.WriteLine("The month is not in the first Quarter");
+            #endregion
+
+            #region Switch 
+            //(jumb table)
+            //Console.WriteLine("Please Enter month number: ");
+            //int MonthNum = int.Parse(Console.ReadLine());
+            //switch (MonthNum)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Hello Jenuary");
+            //        break;
+            //    case 2:
+            //    Console.WriteLine("Hello febraury");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Hello March");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Hello April");
+            //        break;
+            //    default:
+            //        Console.WriteLine("The month is not in the first Quarter");
+            //        break;
+            //}
+
+            //==============================================
+
+            //Console.WriteLine("Please Enter budgt: ");
+            //int budget = Convert.ToInt32(Console.ReadLine());
+            //switch (budget)
+            //{
+            //    case 3000:
+            //        Console.WriteLine("Option 03");
+            //        goto case 2000;
+            //        break;
+            //    case 2000:
+            //        Console.WriteLine("Option 02");
+            //        goto case 1000;
+            //        break;
+            //    case 1000:
+            //        Console.WriteLine("Option 01");
+            //        break;
+            //}
+            #endregion
+
+            #region Switch statment in  C# 7, C# 8, C#9
+            #region C#7 (when) 
+            //Object input = new Object();
+            //input = "Mohamed";
+            //switch (input)
+            //{
+            //    case int Number when Number > 1 && Number < 10:
+            //        Console.WriteLine($"Integer input: {Number}");
+            //        break;
+            //    case double Number:
+            //        Console.WriteLine($"integer input: {Number}");
+            //        break;
+            //    case string Name:
+            //        Console.WriteLine($"String input: {Name}");
+            //        break;
+            //}
+            #endregion
+
+            #region C# 8
+            //string option = "1";
+            //string msg = "";
+            //msg = option switch
+            //{
+            //    "1" => "Using option 1",
+            //    "2" => "using option 2",
+            //    "3" => "using option 3",
+            //    _ => "using no option"
+            //};
+            //Console.WriteLine(msg);
+            #endregion
+
+            #region C# 9
+            //string option = "1";
+            //string msg = "";
+            //msg = option switch
+            //{
+            //    "1" or "2" => "Using option 1",
+            //    "3" => "using option 3",
+            //    _ => "using no option"
+            //};
+            Console.WriteLine(msg);
+            #endregion
+            #endregion
+            #endregion
+            
+        }
+    }
+}
